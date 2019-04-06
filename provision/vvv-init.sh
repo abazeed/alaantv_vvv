5a45c3b9582e4ca84894b8f0e540cd5f43fb3bc2
Jafar <abazeed@users.noreply.github.com>
2019-04-06 14:57:15 +0400

    Updated vvv-init.sh to install WordPress with our own configs

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -2,17 +2,16 @@
 # Provision WordPress Stable
 
 # fetch the first host as the primary domain. If none is available, generate a default using the site name
-DOMAIN=`get_primary_host "${VVV_SITE_NAME}".test`
+DOMAIN=`get_primary_host loc."${VVV_SITE_NAME}"`
 SITE_TITLE=`get_config_value 'site_title' "${DOMAIN}"`
 WP_VERSION=`get_config_value 'wp_version' 'latest'`
 WP_TYPE=`get_config_value 'wp_type' "single"`
-DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
-DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
+DB_NAME='alaantv_sites'
 
 # Make a database, if we don't already have one
 echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
 mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
-mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO root@localhost IDENTIFIED BY 'root';"
 echo -e "\n DB operations done.\n\n"
 
 # Nginx Logs
@@ -23,14 +22,16 @@ touch ${VVV_PATH_TO_SITE}/log/nginx-access.log
 # Install and configure the latest stable version of WordPress
 if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-load.php" ]]; then
     echo "Downloading WordPress..."
-	noroot wp core download --version="${WP_VERSION}"
+    git clone https://github.com/abazeed/alaantv.git
 fi
 
 if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-config.php" ]]; then
   echo "Configuring WordPress Stable..."
-  noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
+  noroot wp core config --dbname="${DB_NAME}" --dbuser=root --dbpass=root --dbprefix=tv_ --quiet --extra-php <<PHP
 define( 'WP_DEBUG', true );
 define( 'SCRIPT_DEBUG', true );
+define( 'WP_ALLOW_MULTISITE', true );
+
 PHP
 fi
 
@@ -45,7 +46,7 @@ if ! $(noroot wp core is-installed); then
     INSTALL_COMMAND="install"
   fi
 
-  noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.test" --admin_password="password"
+  noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="abazeed@msn.com" --admin_password="admin"
 else
   echo "Updating WordPress Stable..."
   cd ${VVV_PATH_TO_SITE}/public_html
